3a2cb21137aaf7c27f9b829ba0cc9d68a52db9d0
Gustavo Nunes <gustnunes.mp4@gmail.com>
2017-12-06 22:08:04 -0200

    Melhorando a implementacao do metodo imprimirTabuleiro

--- projeto-final/Program.cs
+++ projeto-final/Program.cs
@@ -16,6 +16,8 @@ namespace projeto_final
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
                 tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+
                 Tela.imprimirTabuleiro(tab);
             }
             catch(TabuleiroException e)
